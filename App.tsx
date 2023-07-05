4664af55d50d29e860d3e83cf7a172bdb83c9c8a
joaoricardoM <joao_pia_mds@hotmail.com>
2023-07-05 11:39:30 -0300

    :sparkles: Feat: Added navigation between pages

--- App.tsx
+++ App.tsx
@@ -1,17 +1,15 @@
-import Login from "./src/Login";
-import Cadastro from "./src/Cadastro";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { SSRProvider } from "@react-aria/ssr";
 
 import { TEMAS } from "./src/styles/Themes";
+import Routes from "./src/route";
 
 export default function App() {
   return (
     <SSRProvider>
       <NativeBaseProvider theme={TEMAS}>
         <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
-        {/* <Login /> */}
-        <Cadastro />
+        <Routes />
       </NativeBaseProvider>
     </SSRProvider>
   );
