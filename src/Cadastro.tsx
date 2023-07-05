02810e9eb083e51588070a93bc5fb821d4cdbf34
joaoricardoM <joao_pia_mds@hotmail.com>
2023-07-05 11:36:53 -0300

    refactor: refactor code

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -1,68 +1,34 @@
-import {
-  VStack,
-  Image,
-  Text,
-  Box,
-  Input,
-  FormControl,
-  Button,
-  Link,
-} from "native-base";
+import { VStack, Image, Checkbox, Box, ScrollView, Text } from "native-base";
 import { TouchableOpacity } from "react-native";
 import Logo from "./assets/Logo.png";
 import { Title } from "./components/Title";
 import { Inputs } from "./components/Input";
 import { Buttons } from "./components/Button";
 import { useState } from "react";
+import { sessions } from "./utils/json";
 
-export default function Login() {
+export default function Cadastro() {
   const [session, setSession] = useState(0);
-  const sessions = [
-    {
-      id: 1,
-      title: "Insira alguns dados básicos",
-      text: [
-        {
-          id: 1,
-          label: "Nome",
-          placeholder: "Digite seu nome completo",
-        },
-        {
-          id: 2,
-          label: "Email",
-          placeholder: "Digite seu E-mail",
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "Agora, mais alguns dados sobre você",
-      text: [
-        {
-          id: 1,
-          label: "CEP",
-          placeholder: "Digite seu nome CEP",
-        },
-        {
-          id: 2,
-          label: "Email",
-          placeholder: "Digite seu E-mail",
-        },
-      ],
-    },
-  ];
 
   function nextSession() {
-    setSession(session + 1);
+    if (session < sessions.length - 1) {
+      setSession(session + 1);
+    }
+  }
+
+  function backSession() {
+    if (session > 0) {
+      setSession(session - 1);
+    }
   }
 
   return (
-    <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
-      <Image source={Logo} alt="Logo Voll" />
+    <ScrollView flex={1} p={5}>
+      <Image source={Logo} alt="Logo Voll" alignSelf="center" mt={8} />
 
       <Title> {sessions[session].title} </Title>
       <Box>
-        {sessions[session].text.map((text) => {
+        {sessions[session]?.text?.map((text) => {
           return (
             <Inputs
               label={text.label}
@@ -72,7 +38,27 @@ export default function Login() {
           );
         })}
       </Box>
-      <Buttons onPress={() => nextSession()}>Avançar</Buttons>
-    </VStack>
+      <Box>
+        <Text color="blue.800" fontWeight="bold" fontSize="md" mt={2} mb={2}>
+          {" "}
+          Selecione o plano:
+        </Text>
+        {sessions[session].checkbox.map((checkbox) => {
+          return (
+            <Checkbox key={checkbox.id} value={checkbox.value}>
+              {checkbox.value}
+            </Checkbox>
+          );
+        })}
+      </Box>
+      {session > 0 && (
+        <Buttons onPress={() => backSession()} bgColor={"gray.400"}>
+          Voltar
+        </Buttons>
+      )}
+      <Buttons onPress={() => nextSession()} mt={4} mb={20}>
+        Avançar
+      </Buttons>
+    </ScrollView>
   );
 }
